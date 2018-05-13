3c9ed4e5095b6dcbfc22f9b49080c5d271f2d38b
Backins <backinsmail@gmail.com>
2018-05-13 14:48:44 +0200

    update form

--- views/forms.php
+++ views/forms.php
@@ -8,8 +8,16 @@
                 <h3>Formulaire de base</h3>
                 <form>
                     <div class="form-group-base">
-                        <label for="exampleText">Type text</label>
-                        <input id="exampleText" name="exampleText" type="text" placeholder="Type : text">
+                        <label for="exampleText_firstInput">Label of your input</label>
+                        <input id="exampleText_firstInput" name="exampleText_firstInput" type="text" placeholder="Type : text">
+                        <div class="small-precision-input">Add information about your input text</div>
+
+                        <label for="exampleText_secondInput">Label of your second input without small precision</label>
+                        <input id="exampleText_secondInput" name="exampleText_secondInput" type="text" placeholder="Type : text">
+
+                        <label for="textArea_exampleBase">Textarea resize with OSPAF</label>
+                        <textarea class="textarea-row-6" placeholder="For choose size of your textarea, enter class : textarea-row-(number)"></textarea>
+                        <div class="small-precision-input">Add information about your textarea</div>
                     </div>
                 </form>
             </div>
