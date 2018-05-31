0290567bbffa51fb8d5177e568da84a4162d9fdd
Backins <backinsmail@gmail.com>
2018-05-31 23:53:03 +0200

    Formulaire de base terminé, update du scss des forms et corrections d'un bug sur la grille

--- views/forms.php
+++ views/forms.php
@@ -16,14 +16,14 @@
                         <input id="exampleText_secondInput" name="exampleText_secondInput" type="text" placeholder="Type : text">
 
                         <label for="exampleText_thirdInput">Enter your email</label>
-                        <input id="exampleText_thirdInput" name="exampleText_thirdInput" type="text" placeholder="Type : email">
+                        <input id="exampleText_thirdInput" name="exampleText_thirdInput" type="email" placeholder="Type : email">
                         <div class="small-precision-input">We never keep your information</div>
 
                         <label for="textArea_exampleBase">Textarea resize with OSPAF</label>
-                        <textarea class="textarea-row-6" placeholder="For choose size of your textarea, enter class : textarea-row-(number)"></textarea>
+                        <textarea id="textArea_exampleBase" name="textArea_exampleBase" class="textarea-row-12" placeholder="For choose size of your textarea, enter class : textarea-row-(number)"></textarea>
                         <div class="small-precision-input">Add information about your textarea</div>
 
-                        <input type="submit" value="Submit" class="btn btn-green">
+                        <input type="submit" value="Submit" class="btn btn-green" onclick="return false;">
                     </div>
                 </form>
                 <div class="content-documentation">
@@ -34,7 +34,33 @@
         <span class="tag-html"><</span><span class="tag-html">label</span> <span class="attribut-html">for=</span><span class="value-html">"exampleText_firstInput"</span><span class="tag-html">></span>Label of your input<span class="tag-html"><</span><span class="tag-html">/label></span>
         <span class="tag-html"><</span><span class="tag-html">input</span> <span class="attribut-html">id=</span><span class="value-html">"exampleText_firstInput"</span> <span class="attribut-html">name=</span><span class="value-html">"exampleText_firstInput"</span> <span class="attribut-html">type=</span><span class="value-html">"text"</span> <span class="attribut-html">placeholder=</span><span class="value-html">"Type : text"</span><span class="tag-html">></span>
         <span class="tag-html"><</span><span class="tag-html">div</span> <span class="attribut-html">class=</span><span class="value-html">"small-precision-input"</span><span class="tag-html">></span>Add information about your input text<span class="tag-html"><</span><span class="tag-html">/div></span>
+
+        <span class="tag-html"><</span><span class="tag-html">label</span> <span class="attribut-html">for=</span><span class="value-html">"exampleText_secondInput"</span><span class="tag-html">></span>Label of your second input without small precision<span class="tag-html"><</span><span class="tag-html">/label></span>
+        <span class="tag-html"><</span><span class="tag-html">input</span> <span class="attribut-html">id=</span><span class="value-html">"exampleText_secondInput"</span> <span class="attribut-html">name=</span><span class="value-html">"exampleText_secondInput"</span> <span class="attribut-html">type=</span><span class="value-html">"text"</span> <span class="attribut-html">placeholder=</span><span class="value-html">"Type : text"</span><span class="tag-html">></span>
+
+        <span class="tag-html"><</span><span class="tag-html">label</span> <span class="attribut-html">for=</span><span class="value-html">"exampleText_thirdInput"</span><span class="tag-html">></span>Enter your email<span class="tag-html"><</span><span class="tag-html">/label></span>
+        <span class="tag-html"><</span><span class="tag-html">input</span> <span class="attribut-html">id=</span><span class="value-html">"exampleText_thirdInput"</span> <span class="attribut-html">name=</span><span class="value-html">"exampleText_thirdInput"</span> <span class="attribut-html">type=</span><span class="value-html">"email"</span> <span class="attribut-html">placeholder=</span><span class="value-html">"Type : email"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">div</span> <span class="attribut-html">class=</span><span class="value-html">"small-precision-input"</span><span class="tag-html">></span>We never keep your information<span class="tag-html"><</span><span class="tag-html">/div></span>
+
+        <span class="tag-html"><</span><span class="tag-html">label</span> <span class="attribut-html">for=</span><span class="value-html">"textArea_exampleBase"</span><span class="tag-html">></span>Textarea resize with OSPAF<span class="tag-html"><</span><span class="tag-html">/label></span>
+        <span class="tag-html"><</span><span class="tag-html">textarea</span> <span class="attribut-html">id=</span><span class="value-html">"textArea_exampleBase"</span> <span class="attribut-html">name=</span><span class="value-html">"textArea_exampleBase"</span> <span class="attribut-html">class=</span><span class="value-html">"textarea-row-6"</span>
+        <span class="attribut-html">placeholder=</span><span class="value-html">"For choose size of your textarea, enter class : textarea-row-(number)"</span><span class="tag-html">></span><span class="tag-html"><</span><span class="tag-html">/textarea></span>
+        <span class="tag-html"><</span><span class="tag-html">div</span> <span class="attribut-html">class=</span><span class="value-html">"small-precision-input"</span><span class="tag-html">></span>Add information about your textarea<span class="tag-html"><</span><span class="tag-html">/div></span>
+
+        <span class="tag-html"><</span><span class="tag-html">input</span> <span class="attribut-html">type=</span><span class="value-html">"submit"</span> <span class="attribut-html">value=</span><span class="value-html">"Submit"</span> <span class="attribut-html">class=</span><span class="value-html">"btn btn-green"</span><span class="tag-html">></span>
+    <span class="tag-html"><</span><span class="tag-html">/div></span>
+<span class="tag-html"><</span><span class="tag-html">/form></span>
 </pre>
+                    <h3>Resize text area</h3>
+                    <p>To resise our textarea, please use this class name :</p>
+                    <ul class="ul-liste-textarea">
+                        <li><span class="value-html">textarea-row-1</span></li>
+                        <li><span class="value-html">textarea-row-2</span></li>
+                        <li><span class="value-html">textarea-row-3</span></li>
+                        <li><span class="value-html">textarea-row-4</span></li>
+                        <li><span class="value-html">textarea-row-6</span></li>
+                        <li><span class="value-html">textarea-row-12</span></li>
+                    </ul>
                 </div>
             </div>
             <div class="footer-col">
