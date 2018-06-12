8b78dab11fdf1bc1962a85d513d116f2ced3b96c
Backins <backinsmail@gmail.com>
2018-06-12 21:42:50 +0200

    Ajout de la pagination

--- views/pagination.php
+++ views/pagination.php
@@ -26,7 +26,7 @@
                 </ul>
                 <h3>Un peu de couleur</h3>
                 <p>
-                    Il suffit simplement d'appliquer le nom des couleurs de nos boutons
+                    Il suffit simplement d'appliquer le nom des couleurs de nos boutons, il n'existe en revanche pas de pagination pour la valeur default et white.
                 </p>
                 <ul class="pagination tosle">
                     <li class="item">
@@ -67,14 +67,95 @@
                 <div class="content-documentation">
                     <p>Pour utiliser notre système de pagination basique</p>
 <pre>
-<span class="tag-html"><</span><span class="tag-html">ul</span><span class="tag-html">></span>
-
+<span class="tag-html"><</span><span class="tag-html">ul</span> <span class="attribut-html">class=</span><span class="value-html">"pagination"</span><span class="tag-html">></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page material-icons"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            first_page
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item active"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            1
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            2
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page material-icons"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            last_page
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
+<span class="tag-html"><</span><span class="tag-html">/ul></span>
+</pre>
+                    <p>Pour utiliser notre système de pagination en coueur, il suffit de rajouter le nom dans la class de la balise <span class="tag-html"><</span><span class="tag-html">ul></span></p>
+<pre>
+<span class="tag-html"><</span><span class="tag-html">ul</span> <span class="attribut-html">class=</span><span class="value-html">"pagination tosle"</span><span class="tag-html">></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page material-icons"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            first_page
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item active"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            1
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            2
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
+    <span class="tag-html"><</span><span class="tag-html">li</span> <span class="attribut-html">class=</span><span class="value-html">"item"</span><span class="tag-html">></span>
+        <span class="tag-html"><</span><span class="tag-html">a</span> <span class="attribut-html">class=</span><span class="value-html">"link-page material-icons"</span> <span class="attribut-html">href=</span><span class="value-html">"#"</span><span class="tag-html">></span>
+            last_page
+        <span class="tag-html"><</span><span class="tag-html">/a></span>
+    <span class="tag-html"><</span><span class="tag-html">/li></span>
 <span class="tag-html"><</span><span class="tag-html">/ul></span>
 </pre>
                     <h3>Pagination et positionnement</h3>
-                    <p>Pour placer votre pagination autrement</p>
+                    <p>Pour placer votre pagination autrement, il suffit de placer l'une des class à la balise <span class="tag-html"><</span><span class="tag-html">ul></span></p>
+                    <ul class="pagination justify-center">
+                        <li class="item">
+                            <a class="link-page material-icons" href="#">
+                                first_page
+                            </a>
+                        </li>
+                        <li class="item active">
+                            <a class="link-page" href="#">1</a>
+                        </li>
+                        <li class="item">
+                            <a class="link-page" href="#">2</a>
+                        </li>
+                        <li class="item">
+                            <a class="link-page material-icons" href="#">
+                                last_page
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="pagination justify-right">
+                        <li class="item">
+                            <a class="link-page material-icons" href="#">
+                                first_page
+                            </a>
+                        </li>
+                        <li class="item active">
+                            <a class="link-page" href="#">1</a>
+                        </li>
+                        <li class="item">
+                            <a class="link-page" href="#">2</a>
+                        </li>
+                        <li class="item">
+                            <a class="link-page material-icons" href="#">
+                                last_page
+                            </a>
+                        </li>
+                    </ul>
                     <ul class="ul-liste-textarea">
-                        <li><span class="value-html">textarea-col-1</span></li>
+                        <li><span class="value-html">justify-center</span> pour centrer</li>
+                        <li><span class="value-html">justify-right</span> pour aligner à droite</li>
                     </ul>
                 </div>
             </div>
